cb3ed77868bc6ecd830f177a4d0763df4813376b
Rylie Johnson <paintedflame@gmail.com>
2018-06-04 20:28:19 -0500

    gets the logic for the waitlist and tables

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(__dirname));
 
 //establishes the arrays
 var tables = [];
@@ -45,32 +46,39 @@ app.post("/api/tables", function (req, res) {
     // This works because of our body-parser middleware
     var newTable = req.body;
 
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newTable.routeName = newTable.customerName.replace(/\s+/g, "").toLowerCase();
+    // // Using a RegEx Pattern to remove spaces from newCharacter
+    // // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    // newTable. = newTable.customerName.replace(/\s+/g, "").toLowerCase();
 
     console.log(newTable);
 
-    tables.push(newTable);
+    if(tables.length < 5){
+        tables.push(newTable);
+        res.json(true);
+    }
+    else {
+        waitList.push(newTable);
+        res.json(false);
+    }
 
     res.json(newTable);
 });
 
-app.post("/api/waitlist", function (req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body-parser middleware
-    var newTable = req.body;
+// app.post("/api/waitlist", function (req, res) {
+//     // req.body hosts is equal to the JSON post sent from the user
+//     // This works because of our body-parser middleware
+//     var newTable = req.body;
 
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newTable.routeName = newTable.customerName.replace(/\s+/g, "").toLowerCase();
+//     // Using a RegEx Pattern to remove spaces from newCharacter
+//     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+//     newTable.customerID = newTable.customerName.replace(/\s+/g, "").toLowerCase();
 
-    console.log(newTable);
+//     console.log(newTable);
 
-    waitlist.push(newTable);
+//     waitlist.push(newTable);
 
-    res.json(newTable);
-});
+//     res.json(newTable);
+// });
 
 // Starts the server to begin listening
 // =============================================================
